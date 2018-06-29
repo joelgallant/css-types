23d809082850fd633c3a0394fdf8693f6487a982
Joel Gallant <joel@joelgallant.me>
2018-06-29 12:10:35 -0600

    More parsing

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,40 @@ use std::io::Read;
 
 #[macro_use] extern crate nom;
 
-type TypeName<'a> = &'a str;
-named!(type_name(&str) -> TypeName, ws!(nom::alphanumeric));
+// A
+named!(type_name(&str) -> &str, ws!(nom::alphanumeric));
+
+// type A
+named!(type_start(&str) -> &str,
+    fold_many1!(
+        tuple!(
+            tag!("type"),
+            tag!(" "),
+            type_name
+        ),
+        "",
+        | _, (_, _, name) | name
+    )
+);
+
+// extends A, B, C
+named!(type_extends(&str) -> Vec<String>,
+    fold_many1!(
+        pair!(
+            ws!(tag!("extends")),
+            many1!(
+                pair!(
+                    type_name,
+                    opt!(ws!(tag!(",")))
+                )
+            )
+        ),
+        Vec::new(),
+        | _, (_, list): (&str, Vec<(&str, Option<&str>)>) | {
+            list.iter().map(| (name, _) | name.to_string()).collect()
+        }
+    )
+);
 
 #[derive(Debug)]
 struct TypeDef {
@@ -12,34 +44,25 @@ struct TypeDef {
     extends: Vec<String>,
 }
 
-named!(type_def<&str, TypeDef>,
-    fold_many0!(
-        pair!(
-            pair!(
-                ws!(tag!("type")),
-                type_name // TypeName
-            ),
-            opt!(pair!(
-                ws!(tag!("extends")),
-                many0!( // BaseNameA, BaseNameB
-                    pair!(
-                        type_name,
-                        opt!(tag!(","))
-                    )
-                )
-            ))
-        ),
-        TypeDef { name: String::new(), extends: vec![] },
-        | _, ((_, name), extends): ((&str, &str), Option<(&str, Vec<(&str, Option<&str>)>)>) | -> TypeDef {
-            TypeDef {
-                name: name.to_string(),
-                extends: match extends {
-                    Some((_, extends)) => {
-                        extends.iter().map(| (name, _) | name.to_string()).collect()
-                    },
-                    None => vec![],
-                },
-            }
+named!(type_defs(&str) -> Vec<TypeDef>,
+    fold_many1!(
+        complete!(ws!(tuple!(
+            type_start,
+            opt!(type_extends),
+            ws!(tag!(";"))
+        ))),
+        Vec::new(),
+        | mut acc: Vec<TypeDef>, (name, extends, _): (&str, Option<Vec<String>>, &str) | {
+            acc.push(
+                TypeDef {
+                    name: name.to_string(),
+                    extends: match extends {
+                        Some(extends) => extends,
+                        None => vec![]
+                    }
+                }
+            );
+            acc
         }
     )
 );
@@ -49,5 +72,8 @@ fn main() {
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
 
-    println!("{:?}", type_def(&contents).unwrap().1);
+    match type_defs(&contents) {
+        Ok((_, res)) => println!("{:?}", res),
+        Err(err) => println!("err: {:?}", err),
+    }
 }
